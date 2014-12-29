4ee77df5c2d49431826be51df27f9e2465e5ba6d
shaaaaaan <shan.sunny@hotmail.com>
2014-12-29 20:55:41 +0300

    JavaScript First Dynamic Page
    'N' No of Cans dynamic page

---
+++ JavaScript/mainjs.js
@@ -0,0 +1,7 @@
+var writeOut = "";
+var noOfCans = 99;
+while ( --noOfCans >= 1 )	
+{
+	writeOut = writeOut + noOfCans + " Cans on the wall. Take one out, pass it around.<br/>";
+}
+document.write(writeOut);
